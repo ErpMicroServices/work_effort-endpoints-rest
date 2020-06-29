c42714c0070e8ecf46917393bef6fb025a59583e
Jim Barrows <Jim.Barrows@gmail.com>
2020-06-29 10:05:27 -0500

    Initial import.

---
+++ src/main/java/org/erpmicroservices/work_effort/endpoints/rest/WorkEffortEndpointsRestApplication.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.work_effort.endpoints.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkEffortEndpointsRestApplication {
+
+ public static void main(String[] args) {
+	SpringApplication.run(WorkEffortEndpointsRestApplication.class, args);
+ }
+
+}
---
+++ src/test/java/org/erpmicroservices/work_effort/endpoints/rest/WorkEffortEndpointsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.work_effort.endpoints.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkEffortEndpointsRestApplicationTests {
+
+ @Test
+ void contextLoads() {
+ }
+
+}
